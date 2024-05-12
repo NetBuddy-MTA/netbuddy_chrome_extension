08a2ef6ca189c77e95e186fbd82c9050266c5cff
OE100 <omer.er10@gmail.com>
2024-05-12 17:29:50 +0300

    refactored data types into separate file

---
+++ src/background/data.ts
@@ -0,0 +1,30 @@
+export type Variable = {
+  origName: string,
+  name: string,
+  type: string,
+  optional: boolean,
+  defaultValue?: unknown
+}
+
+export type Action = {
+  ActionString: string,
+  inputs: Variable[],
+  outputs: Variable[]
+};
+
+export type ActionParams = {
+  inputs: Variable[],
+  outputs: Variable[]
+}
+
+export type Sequence = {
+  id: string,
+  name: string,
+  description: string,
+  actions: Action[]
+};
+
+export type ActionResult = {
+  action: number,
+  data: JSON
+};
