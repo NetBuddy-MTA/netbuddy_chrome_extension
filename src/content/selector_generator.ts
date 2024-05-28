2799a2c0bf6226eced587e376271a12043d643d0
OE100 <omer.er10@gmail.com>
2024-05-28 12:27:07 +0300

    changed the selector generator to work with the new selector data type

--- src/content/selector_generator.ts
+++ src/content/selector_generator.ts
@@ -1,3 +1,10 @@
+import {Selector} from "../shared/data.ts";
+
+// remove the listener once any tab creates a new selector
+chrome.storage.session.onChanged.addListener(changes => {
+  'Selector' in changes && changes.Selector.newValue as Selector && removeListener();
+});
+
 // listener for messages from extension
 chrome.runtime.onMessage.addListener((message) => {
   message as string && message === 'StartGetSelector' && getXPathForElement();
@@ -5,21 +12,17 @@ chrome.runtime.onMessage.addListener((message) => {
 });
 
 // Get the XPath for the element clicked
-export function getXPathForElement(storeResult?: string) {
+export function getXPathForElement() {
   document.body.style.cursor = 'crosshair';
-  document.addEventListener('contextmenu', (event: MouseEvent) => handleClick(event, storeResult));
+  document.addEventListener('contextmenu', handleClick);
 }
 
 // handle click event
-async function handleClick(event: MouseEvent, storeResult?: string) {
+async function handleClick(event: MouseEvent) {
   const element = document.elementFromPoint(event.clientX, event.clientY);
   removeListener();
-  let xpath = getElementXPath(element);
-  if (xpath) {
-    xpath = "/" + xpath;
-    await navigator.clipboard.writeText(xpath);
-  }
-  storeResult && await chrome.storage.local.set({storeResult: xpath});
+  const selector = getElementSelector(element);
+  await chrome.storage.local.set({Selector: selector});
 }
 
 // doing all the things that need doing when removing listener
@@ -28,41 +31,46 @@ function removeListener() {
   document.body.style.cursor = 'default';
 }
 
-// Get the XPath for the element
-function getElementXPath(element: Element | null): string | null {
-  if (element && element.id)
-    return '//*[@id="' + element.id + '"]';
-  else
-    return getElementTreeXPath(element);
-}
+// get the selector for this element
+function getElementSelector(element: Element | null): Selector | null {
+  if (!element) return null;
+  
+  const selector: Selector = {
+    id: "",
+    name: "",
+    url: location.href,
+    stages: []
+  };
 
-// Get the tree XPath for the element
-function getElementTreeXPath(element: Element | null): string | null {
-  if (!element)
-    return null;
-
-  const paths: string[] = [];
-
+  // build selector from the element
   for (; element && element.nodeType == Node.ELEMENT_NODE; element = element.parentElement) {
-    let index = 0;
-    let hasFollowingSiblings = false;
-    for (let sibling = element.previousSibling; sibling; sibling = sibling.previousSibling) {
-      if (sibling.nodeType == Node.DOCUMENT_TYPE_NODE)
-        continue;
-
-      if (sibling.nodeName == element.nodeName)
-        ++index;
-    }
-
-    for (let sibling = element.nextSibling; sibling && !hasFollowingSiblings; sibling = sibling.nextSibling) {
-      if (sibling.nodeName == element.nodeName)
-        hasFollowingSiblings = true;
-    }
-
-    const tagName = (element.prefix ? element.prefix + ":" : "") + element.localName;
-    const pathIndex = (index || hasFollowingSiblings ? "[" + (index + 1) + "]" : "");
-    paths.splice(0, 0, tagName + pathIndex);
+    const tag = (element.prefix ? element.prefix + ":" : "") + element.localName;
+    
+    // get all attributes of the element into maps
+    const attrs = [...element.attributes];
+    const attributes = attrs.reduce((acc, attr) => {
+      acc.set(attr.name, attr.value);
+      return acc;
+    }, new Map<string, string>);
+    const useAttributes = attrs.reduce((acc, attr) => {
+      if (attr.name === "id") acc.set(attr.value, true);
+      return acc;
+    }, new Map<string, boolean>);
+    
+    selector.stages.push({
+      tag,
+      inUse: true,
+      attributes,
+      useAttributes 
+    });
   }
-
-  return paths.length ? "/" + paths.join("/") : null;
+  
+  // set the inUse flag for each stage (from the last element with an id to the end)
+  let use = true;
+  for (let i = selector.stages.length - 1; i >= 0; i--) {
+    selector.stages[i].inUse = use;
+    if (use && selector.stages[i].attributes.has("id")) use = false;
+  } 
+  
+  return selector;
 }
