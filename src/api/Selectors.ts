27613f3d7e34c2b985ce2b88baf9b07feb787ddf
OE100 <omer.er10@gmail.com>
2024-05-29 20:34:03 +0300

    changed api

--- src/api/Selectors.ts
+++ src/api/Selectors.ts
@@ -2,7 +2,7 @@ import {Selector} from "../shared/data.ts";
 
 export async function SaveSelector(selector: Selector) {
   return await fetch(new URL('https://localhost:7298/selectors'), {
-    method: "POST",
+    method: "PUT",
     body: JSON.stringify({...selector, id: selector.id === "" ? "00000000-0000-0000-0000-000000000000" : selector.id}),
     headers: {
       'Content-Type': 'application/json'
