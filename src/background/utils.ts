e3db0f8a8469cd2a78bd0fb4d4b3f131e50c0a5b
OE100 <omer.er10@gmail.com>
2024-05-30 19:38:11 +0300

    added function that surrounds an area with red border, for debugging.

--- src/background/utils.ts
+++ src/background/utils.ts
@@ -12,3 +12,21 @@ export const InitSequenceAlarm = (callback: () => void) => {
     }
   });
 }
+
+// surrounds an area of the page with red borders
+export function addDebugRects(rects: DOMRect[]) {
+  for (const rect of rects) {
+    const tableRectDiv = document.createElement("div");
+    tableRectDiv.style.position = "absolute";
+    tableRectDiv.style.border = "1px solid red";
+    const scrollTop = document.documentElement.scrollTop || document.body.scrollTop;
+    const scrollLeft = document.documentElement.scrollLeft || document.body.scrollLeft;
+    tableRectDiv.style.margin = tableRectDiv.style.padding = "0";
+    tableRectDiv.style.top = `${rect.top + scrollTop}px`;
+    tableRectDiv.style.left = `${rect.left + scrollLeft}px`;
+    // We want rect.width to be the border width, so content width is 2px less.
+    tableRectDiv.style.width = `${rect.width - 2}px`;
+    tableRectDiv.style.height = `${rect.height - 2}px`;
+    document.body.appendChild(tableRectDiv);
+  }
+}
