52c7e50da2f45a491357e7d28a8402f6da734722
OE100 <omer.er10@gmail.com>
2024-06-19 01:20:14 +0300

    added debug console logs and an action to the mapping

--- src/content/content_script.ts
+++ src/content/content_script.ts
@@ -1,10 +1,20 @@
-import {clickElement, findElementsBySelector, readElementText, writeElementText} from "./content_actions.ts";
+import {
+  clickElement,
+  findElementBySelector,
+  findElementsBySelector,
+  readElementText,
+  writeElementText
+} from "./content_actions.ts";
 import {Action} from "../shared/data.ts";
 
 // adding listener for messages from extension
 chrome.runtime.onMessage.addListener(async (message, _sender, sendResponse) => {
   if (message as {action: Action, context: Record<string, unknown>} && message.action && message.context) {
     const {action, context} = message;
+    console.log("action:");
+    console.log(action);
+    console.log("context:");
+    console.log(context);
     switch (action.actionString) {
       case 'ClickElement':
         sendResponse(clickElement(action, context));
@@ -20,6 +30,10 @@ chrome.runtime.onMessage.addListener(async (message, _sender, sendResponse) => {
         
       case 'FindElementsBySelector':
         sendResponse(findElementsBySelector(action, context));
+        break;
+        
+      case 'FindElementBySelector':
+        sendResponse(findElementBySelector(action, context));
         break;
     }
   }
