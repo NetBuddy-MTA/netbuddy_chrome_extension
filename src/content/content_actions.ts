e6b9b65c9c9bbd7b25eb84c402ee6f445f94d3b3
OE100 <omer.er10@gmail.com>
2024-06-18 18:35:07 +0300

    refactored content script actions to work with records (and to work at all)

--- src/content/content_actions.ts
+++ src/content/content_actions.ts
@@ -1,12 +1,13 @@
 // Press the parameter element
-import {Action} from "../shared/data.ts";
+import {Action, Selector} from "../shared/data.ts";
+import {selectorToString} from "./utils.ts";
 
-export function clickElement(action: Action, context: Map<string, unknown>) {
+export function clickElement(action: Action, context: Record<string, unknown>) {
   // find the element input variable in the context
   const elementInput = action.inputs.find(value => value.originalName === 'Element');
   if (elementInput) {
     // get the element from the context
-    const element = context.get(elementInput.name) as HTMLElement;
+    const element = context[elementInput.name] as HTMLElement;
     // click the element
     element.click();
   }
@@ -14,12 +15,12 @@ export function clickElement(action: Action, context: Map<string, unknown>) {
 }
 
 // read the text contents of the element
-export function readElementText(action: Action, context: Map<string, unknown>) {
+export function readElementText(action: Action, context: Record<string, unknown>) {
   // find the element input variable in the context
   const elementInput = action.inputs.find(value => value.originalName === 'Element');
   if (elementInput) {
     // get the element from the context
-    const element = context.get(elementInput.name) as HTMLElement;
+    const element = context[elementInput.name] as HTMLElement;
     const content = element instanceof HTMLInputElement ? element.value : element.innerText;
     // get the text output variable in the context
     const textOutput = action.outputs.find(value => value.originalName === 'Element Text');
@@ -29,16 +30,16 @@ export function readElementText(action: Action, context: Map<string, unknown>) {
 }
 
 // write text to element if it is an input element
-export function writeElementText(action: Action, context: Map<string, unknown>) {
+export function writeElementText(action: Action, context: Record<string, unknown>) {
   // find the element input variable in the context
   const elementInput = action.inputs.find(value => value.originalName === 'Element');
   // find the text input variable in the context
   const textInput = action.inputs.find(value => value.originalName === 'Text');
   if (elementInput && textInput) {
     // get the element from the context
-    const element = context.get(elementInput.name) as HTMLElement;
+    const element = context[elementInput.name] as HTMLElement;
     // get the text from the context
-    const text = context.get(textInput.name) as string;
+    const text = context[textInput.name] as string;
     // check if the element is an input element
     const result = element instanceof HTMLInputElement;
     // if the element is an input element, set the value to the text
@@ -54,9 +55,15 @@ export function writeElementText(action: Action, context: Map<string, unknown>) 
 }
 
 // Get the elements that match the xpath query
-export function getElementsByXPath(xpath: string, storeResult?: string) {
-  console.log("getting elements by xpath", xpath);
-  const elements = document.evaluate(xpath, document, null, XPathResult.ANY_TYPE, null);
+export function findElementsBySelector(action: Action, context: Record<string, unknown>) {
+  // find the selector input variable in the context
+  const selectorInput = action.inputs.find(value => value.originalName === 'Selector');
+  // if the selector or tab input is not found, return an empty object
+  if (!selectorInput) return {};
+  // get the selector from the context
+  const selector = context[selectorInput.name] as Selector;
+  // get the matching elements
+  const elements = document.evaluate(selectorToString(selector), document, null, XPathResult.ANY_TYPE, null);
   const results = [];
   let element = elements.iterateNext();
   while (element) {
@@ -69,7 +76,5 @@ export function getElementsByXPath(xpath: string, storeResult?: string) {
   results.forEach((element) => {
     element.style.border = '3px solid red';
   });
-
-  storeResult && chrome.storage.local.set({storeResult: results});
   return results;
 }
