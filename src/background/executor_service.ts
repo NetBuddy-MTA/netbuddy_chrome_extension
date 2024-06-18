36c4c1d2ea4a8b05bfbb054747c4aa316c08fdb1
OE100 <omer.er10@gmail.com>
2024-06-18 18:35:43 +0300

    partially implemented the run logic

--- src/background/executor_service.ts
+++ src/background/executor_service.ts
@@ -1,4 +1,4 @@
-import {Action, ActionResult, Sequence} from "../shared/data.ts"
+import {Action, ActionResult} from "../shared/data.ts"
 import {
   closeWindow,
   contentScriptAction,
@@ -9,6 +9,7 @@ import {
 } from "./background_actions.ts";
 import {InitSequenceAlarm} from "./utils.ts";
 import {menuItems} from "./context_menu_items.ts";
+import {GetQueue, Pipeline} from "../api/runQueue.ts";
 
 // add context menu items
 chrome.runtime.onInstalled.addListener(() => {
@@ -21,41 +22,13 @@ chrome.runtime.onInstalled.addListener(() => {
   });
 });
 
-// try to run a sequence from the run queue every roughly 4 seconds
-InitSequenceAlarm(runSequence);
-
-// run queue for sequences
-const runQueue: Sequence[] = [];
-let running = false;
-
-// request sequence from server by id
-async function getSequence(id: string) {
-  const res = await fetch(`https://localhost:7298/execution/sequences/${id}`);
-  return await res.json() as Sequence;
-}
-
-// add listener for https responses from the netbuddy website
-chrome.webRequest.onCompleted.addListener(async details => {
-  // if no headers, return
-  if (!details.responseHeaders) return;
-  // check if the request contains the header 'X-Netbuddy-Sequence'
-  const sequenceHeader = details.responseHeaders.find(header => header.name === 'X-Netbuddy-Sequence');
-  // if no sequence header, return
-  if (!sequenceHeader || !sequenceHeader.value) return;
-  // parse the sequence header
-  const sequenceId = sequenceHeader.value;
-  // request the sequence from the server
-  const sequence = await getSequence(sequenceId);
-  // add the sequence to the run queue
-  runQueue.push(sequence);
-}, {urls: ["https://localhost:7298/*"]});
-
 // interpret the action and execute it
-async function executeAction(action: Action, context: Map<string, unknown>) {
+async function executeAction(action: Action, context: Record<string, unknown>) {
   // todo: implement action execution
   // load initial values to context if value wasn't assigned yet
-  action.inputs.forEach(input => 
-    input.defaultValue && !context.has(input.name) && context.set(input.name, input.defaultValue));
+  action.inputs.forEach(input => {
+    if (input.defaultValue && !(input.name in context)) context[input.name] = input.defaultValue;
+  });
   
   switch (action.actionString) {
     case "CreateWindow":
@@ -87,14 +60,23 @@ async function executeAction(action: Action, context: Map<string, unknown>) {
   return {} as ActionResult;
 }
 
-// runs a sequence from the run queue
-async function runSequence() {
-  if (!running && runQueue.length > 0) {
+// gets the first to run from the run queue and tries to run it
+let running = false;
+const runSequence = async () => {
+  // update run queue
+  const response = await GetQueue();
+  if (!response.ok) return;
+  const pipeline: Pipeline = await response.json(); 
+  // try to run a sequence
+  if (!running) {
+    // todo: try and get confirmation for the pipeline
+    
+    // start running
     running = true;
-    const sequence = runQueue.shift();
+    const sequence = pipeline.sequence;
     if (sequence) {
       // create a new context for the sequence
-      const context = new Map<string, unknown>();
+      const context = pipeline.context;
       
       // execute each action in the sequence
       for (const action of sequence.actions) {
@@ -106,3 +88,6 @@ async function runSequence() {
     running = false;
   }
 }
+
+// try to run a sequence from the run queue every roughly 4 seconds
+InitSequenceAlarm(runSequence);
